7f131134ea32cfcf2598cb98e31936ff78a278cb
mRSun15 <3150105645@zju.edu.cn>
2018-06-03 16:42:24 +0800

    modified the DDR2

--- src/platform/SWORD4/Top.v
+++ src/platform/SWORD4/Top.v
@@ -1,48 +1,51 @@
 `timescale 1ns / 1ps
-/**
- * The top module of SoC project.
- * 
- * @auther Yunye Pu
- */
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/23/2016 07:21:03 PM
+// Design Name: 
+// Module Name: Top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 module Top(
-//	input clk, 
-	input rstn,
-	input sysclk_p,
-	input sysclk_n,
-	input [15:0] SW, inout [4:0] btnX, inout [4:0] btnY,
-	output [2:0] seg_sout, output [1:0] led_sout,
+	input clk, input rstn,
+	input [15:0] SW, input [4:0] BTN,
+	output [7:0] segment, output [7:0] anode, output [15:0] LED,
 	output [11:0] VGAColor, output HSync, output VSync,
 	inout ps2Clk, inout ps2Dat,
 	input uartRx, output uartTx,
-	inout [3:0] sdDat, inout sdCmd, input sdCd, output sdClk,
-	
-	//Arduino basic I/O
-	output [7:0] segment,
-	output [1:0] anode,
-	output [7:0] LED,
-	inout btnL, input btnR,
-	
-	//DDR3 interface
-	output [13:0] ddr3_addr,
-	output [2:0] ddr3_ba,
-	output ddr3_cas_n,
-	output [0:0] ddr3_ck_n,
-	output [0:0] ddr3_ck_p,
-	output [0:0] ddr3_cke,
-	output ddr3_ras_n,
-	output ddr3_reset_n,
-	output ddr3_we_n,
-	inout [31:0] ddr3_dq,
-	inout [3:0] ddr3_dqs_n,
-	inout [3:0] ddr3_dqs_p,
-	output ddr3_cs_n,
-	output [3:0] ddr3_dm,
-	output [0:0] ddr3_odt,
-	output sdRst
-);
+	inout [3:0] sdDat, inout sdCmd, input sdCd, output sdClk, output sdRst,
 
+	//DDR2 interface
+	output [12:0] ddr2_addr,
+	output [2:0] ddr2_ba,
+	output ddr2_cas_n,
+	output [0:0] ddr2_ck_n,
+	output [0:0] ddr2_ck_p,
+	output [0:0] ddr2_cke,
+	output ddr2_ras_n,
+//	output ddr2_reset_n,
+	output ddr2_we_n,
+	inout [15:0] ddr2_dq,
+	inout [1:0] ddr2_dqs_n,
+	inout [1:0] ddr2_dqs_p,
+	output ddr2_cs_n,
+	output [1:0] ddr2_dm,
+	output [0:0] ddr2_odt
+);
 	//Unused I/O signals
-	wire buzzer;
+	
 	//Clock and reset signal
 	wire clk_100M, clkCPU, clkVGA, clkDDR, globlRst;
 	
@@ -70,7 +73,7 @@ module Top(
 	//VGA control registers
 	wire [31:0] vgaCtrlReg0, vgaCtrlReg1;
 	
-	CPUCacheTop core(
+	CPUCacheTop #(.CLKCPU_PERIOD(20), .CLKDDR_PERIOD(12)) core (
 		.clkCPU(clkCPU), .clkDDR(clkDDR), .rst(globlRst), .interrupt(cpuInterrupt),
 		.addrIBus(addrIBus), .dinIBus(dinIBus), .stbIBus(), .nakIBus(1'b0),
 		.addrDBus(addrDBus), .doutDBus(doutDBus), .dinDBus(dinDBus),
@@ -80,10 +83,9 @@ module Top(
 		.dbg_vPC(dbg_vPC), .dbg_vAddr(dbg_vAddr), .dbg_IDPC(), .dbg_EXPC(), .dbg_MEMPC()
 	);
 	
-	Infrastructure_Sword #(.DEBUG(1'b1)) infrastructure(.clk(clkDDR),  .rstn(rstn),
+	Infrastructure_Nexys4 #(.DEBUG(1'b1)) infrastructure(.clk(clkDDR),  .rstn(rstn),
 		.clk_100M(clk_100M), .clkCPU(clkCPU), .clkVGA(clkVGA), .globlRst(globlRst),
-		.SW(SW), .btnX(btnX), .btnY(btnY), .btnL(btnL), .btnR(btnR),
-		.segment(segment), .anode(anode), .led_sout(led_sout), .seg_sout(seg_sout),
+		.SW(SW), .BTN(BTN), .digitSeg(segment), .digitAnode(anode), .LED(LED),
 		.ps2Clk(ps2Clk), .ps2Dat(ps2Dat), .uartRx(uartRx), .uartTx(uartTx),
 		
 		.vgaCtrlReg0(vgaCtrlReg0), .vgaCtrlReg1(vgaCtrlReg1),
@@ -96,7 +98,6 @@ module Top(
 //		.dbg_flags({dmDBus, stbDBus, progMemEN, cVramEN, gVramEN, ioEN, sdCtrlEN, sdDataEN, cpuInterrupt[4:0]})
 		.dbg_flags({dmDBus, stbDBus, stbDDR, cycDDR, weDDR, ackDDR, dbg_ddrState, cpuInterrupt[3:0]})
 	);
-	assign LED = {stbDBus, stbDDR, cycDDR, weDDR, ackDDR, dbg_ddrState};
 	
 	VGADevice #(.GRAPHIC_VRAM(0)) vga(.clkVGA(clkVGA), .clkMem(clkCPU),
 		.ctrl0(vgaCtrlReg0), .ctrl1(vgaCtrlReg1),
@@ -126,32 +127,30 @@ module Top(
 		.web(4'h0), .enb(1'b1), .doutb(dinIBus),
 		.clkProg(clk_100M), .uartRx(uartRx), .progEN(globlRst));
 	
-	DDR3_wsWrapper ddr3(
-		.sysclk_p(sysclk_p), .sysclk_n(sysclk_n),
-		.clkOut(clkDDR), .rst(globlRst),
+	DDR2_wsWrapper ddr2(.clkIn(clk), .clkOut(clkDDR), .rst(globlRst),
 		.ws_addr(addrDDR), .ws_din(doutDDR), .ws_dm(dmDDR),
 		.ws_cyc(cycDDR), .ws_stb(stbDDR), .ws_we(weDDR),
 		.ws_dout(dinDDR), .ws_ack(ackDDR),
 		
 		.dbg_state(dbg_ddrState),
 		
-		.ddr3_addr(ddr3_addr),
-		.ddr3_ba(ddr3_ba),
-		.ddr3_cas_n(ddr3_cas_n),
-		.ddr3_ck_n(ddr3_ck_n),
-		.ddr3_ck_p(ddr3_ck_p),
-		.ddr3_cke(ddr3_cke),
-		.ddr3_ras_n(ddr3_ras_n),
-		.ddr3_reset_n(ddr3_reset_n),
-		.ddr3_we_n(ddr3_we_n),
-		.ddr3_dq(ddr3_dq),
-		.ddr3_dqs_n(ddr3_dqs_n),
-		.ddr3_dqs_p(ddr3_dqs_p),
-		.ddr3_cs_n(ddr3_cs_n),
-		.ddr3_dm(ddr3_dm),
-		.ddr3_odt(ddr3_odt)
+		.ddr2_addr(ddr2_addr),
+		.ddr2_ba(ddr2_ba),
+		.ddr2_cas_n(ddr2_cas_n),
+		.ddr2_ck_n(ddr2_ck_n),
+		.ddr2_ck_p(ddr2_ck_p),
+		.ddr2_cke(ddr2_cke),
+		.ddr2_ras_n(ddr2_ras_n),
+//		.ddr2_reset_n(ddr2_reset_n),
+		.ddr2_we_n(ddr2_we_n),
+		.ddr2_dq(ddr2_dq),
+		.ddr2_dqs_n(ddr2_dqs_n),
+		.ddr2_dqs_p(ddr2_dqs_p),
+		.ddr2_cs_n(ddr2_cs_n),
+		.ddr2_dm(ddr2_dm),
+		.ddr2_odt(ddr2_odt)
 	);
-
+	
 	assign buzzer = 1'b1;
-
+	
 endmodule
